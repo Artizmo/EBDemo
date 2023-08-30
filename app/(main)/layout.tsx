9984a040189ced3bb71142169751afc04ef6c24a
legendStraussy <straussy@legendmud.org>
2023-08-30 10:08:00 -0500

    init

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,7 +6,7 @@ export default function HeaderLayout({ children }: LayoutProps) {
   return (
     <div>
       <div className="m-4 flex flex-col">
-        <LogoIcon className="fill-black w-[120px] sm:w-[160px] " />
+        <LogoIcon className="fill-black w-[120px] sm:w-[160px]" />
       </div>
       <div>{children}</div>
     </div>
