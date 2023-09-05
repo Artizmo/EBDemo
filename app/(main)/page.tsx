2942b946151dd26272336e701afaf1bfc1a1a45b
Brian Selvaggio <brian.selvaggio@eddiebauer.com>
2023-09-05 17:12:17 -0500

    more mock out

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,7 +4,7 @@ export default function MainPage() {
   return (
     <div>
       <Image
-        src="/promo.webp"
+        src="https://res.cloudinary.com/de0ei4yhn/image/upload/v1693951141/promo.webp"
         alt=""
         priority
         width={1650}
