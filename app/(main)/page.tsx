2bd2c9c0b3716337be9553f3bf9f974f116d1240
Brian Selvaggio <brian.selvaggio@eddiebauer.com>
2023-09-06 09:24:49 -0500

    wip

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,17 +1,15 @@
 import Image from 'next/image'
-import promoImage from '@/public/promo.webp'
 
 export default async function MainPage() {
 
   return (
     <div>
       <Image
-        src={promoImage}
+        src="https://res.cloudinary.com/de0ei4yhn/image/upload/v1693951141/promo.webp"
         alt=""
         priority
         width={1650}
         height={445}
-        placeholder="blur"
       />
     </div>
   )
