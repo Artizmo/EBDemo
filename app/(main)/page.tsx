1373bf923b659bf22ce065b886003f8d6f503b47
Brian Selvaggio <brian.selvaggio@eddiebauer.com>
2023-09-08 17:27:45 -0500

    wip

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,7 +4,7 @@ export default function MainPage() {
   return (
     <div className="w-[1650px] h-[445px]">
       <Image
-        src="https://res.cloudinary.com/de0ei4yhn/image/upload/v1693951141/EBDemo/promo.webp"
+        src="https://res.cloudinary.com/de0ei4yhn/image/upload/v1694211177/EBDemo/promo.webp"
         alt=""
         sizes="90vw"
         priority
