1e395224d414b877143264530738cadee2028868
Brian Selvaggio <brian.selvaggio@eddiebauer.com>
2023-09-20 14:47:08 -0500

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   description: 'This is a tech stack demo',
   viewport: {
     width: 'device-width',
-    initialScale: 1,
+    // initialScale: 1,
     maximumScale: 1
   }
 }
