bf3d348f324b37ef093fbb253f5f7482e7f6dcda
Brian Selvaggio <brian.selvaggio@eddiebauer.com>
2023-10-03 00:03:19 -0500

    wip

--- app/(main)/components/scroll-header.tsx
+++ app/(main)/components/scroll-header.tsx
@@ -6,10 +6,10 @@ type ScrollHeaderProps = { children: React.ReactNode };
 
 export default function ScrollHeader({ children }: ScrollHeaderProps)  {
   const scrollDirection = useScrollDirection()
-  
+
   return (
     <div className={`w-full flex absolute flex-1 md:hidden p-2 items-center justify-between bg-transparent
-      ${scrollDirection === 'equal' ? 'bg-transparent' : 'z-[2] ease-in duration-400 bg-white [&_.header-logo]:fill-black [&_.header-icon]:fill-black'} `}
+      ${scrollDirection === 'equal' ? 'ease-in duration-300 bg-transparent' : 'z-[2] bg-white [&_.header-logo]:fill-black [&_.header-icon]:fill-black'} `}
     >
       {children}
     </div>
