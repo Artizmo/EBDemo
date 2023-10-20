39ef903eb4ec2d2c4b1cac54eb5cd179934059d7
Brian Selvaggio <brian.selvaggio@eddiebauer.com>
2023-10-19 19:47:42 -0500

    wip

--- app/(main)/_components/site-footer.tsx
+++ app/(main)/_components/site-footer.tsx
@@ -1,5 +1,49 @@
+import Image from 'next/image'
+import FacebookIcon from "@/components/icons/facebook"
+import InstagramIcon from "@/components/icons/instragram"
+import PinterestIcon from "@/components/icons/pinterest"
+import TwitterIcon from "@/components/icons/twitter"
+import YoutubeIcon from "@/components/icons/youtube"
+
 export default function SiteFooter() {
   return (
-    <div className="relative">site footer</div>
+    <footer className="text-white flex flex-col justify-end w-full h-screen">
+      <Image 
+        src="/images/footer.png"
+        alt=""
+        sizes="90vw"
+        priority
+        width={0}
+        height={0}
+        className="max-h-[28rem] absolute z-[-1] md:hidden w-full h-screen object-cover object-right"
+      />
+      <section className="p-4">
+        <p className="uppercase text-[14px]">follow us</p>
+        <p className="text-[14px] font-light">There is always something more for you to discover.</p>
+      </section>
+      <section className="flex gap-x-6 p-4">
+        <FacebookIcon className="w-[24px] fill-white" />
+        <YoutubeIcon className="w-[24px] fill-white" />
+        <PinterestIcon className="w-[24px] fill-white" />
+        <InstagramIcon className="w-[24px] fill-white" />
+        <TwitterIcon className="w-[24px] fill-white" />
+      </section>
+      <section className="p-4">
+        <div>collapsible A</div>
+        <div>collapsible B</div>
+        <div>collapsible C</div>
+        <div>collapsible D</div>
+        <div>collapsible E</div>
+      </section>
+      <section className="flex justify-between p-4">
+        <p>text 1</p>
+        <p>text 2</p>
+        <p>text 3</p>
+      </section>
+      <section className="flex justify-between p-4">
+        <p>text 1</p>
+        <p>text 2</p>
+      </section>
+    </footer>
   )
 }
---
+++ components/icons/facebook.tsx
@@ -0,0 +1,11 @@
+type FacebookIconProps = {
+  className?: string
+}
+
+export default function FacebookIcon({ className }: FacebookIconProps) {
+  return (
+    <svg className={className} viewBox="0 0 24 24">
+      <path d="M15.833 6.27885H17.75V3.0591C17.4193 3.01522 16.2819 2.9165 14.9572 2.9165C12.1933 2.9165 10.3 4.59304 10.3 7.67441V10.5103H7.25V14.1097H10.3V23.1665H14.0394V14.1105H16.9661L17.4307 10.5111H14.0386V8.03132C14.0394 6.99097 14.3299 6.27885 15.833 6.27885Z" />
+    </svg>
+  )
+};
